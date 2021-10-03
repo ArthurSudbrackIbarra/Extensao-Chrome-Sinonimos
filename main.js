d6b282231efe9a770bfa15e32d492049a9c276b3
MagicManatee <arthur.ibarra@edu.pucrs.br>
2021-10-03 14:57:08 -0300

    Normalização do url.

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 // This function will open the synonyms site url in a new tab,
 // passing the selected word as a parameter.
 
-findSynonyms = function(word){
-    var param = word.selectionText;
-    chrome.tabs.create({ url: `https://www.sinonimos.com.br/${param}/` });
+findSynonyms = function(word) {
+
+    let param = word.selectionText;
+    let url;
+
+    if (param) {
+
+        param = param.normalize("NFD").replace(/[\u0300-\u036f]/g, "");  
+
+        if(param.includes(" ")) {
+            param = param.replace(" ", "+");
+            url = `https://www.sinonimos.com.br/busca.php?q=${param}`;
+        } else {
+            url = `https://www.sinonimos.com.br/${param}/`;
+        }
+        
+    }
+
+    chrome.tabs.create({ url: encodeURI(url) });
+
 };
 
 // The code below creates a room for our extension
